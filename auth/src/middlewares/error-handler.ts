4652fdb4b774023e9370e92bbd37c950e77478d4
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2022-02-21 14:10:21 -0800

    error handling

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 export const errorHandler = (
 	err: Error, 
@@ -6,8 +8,20 @@ export const errorHandler = (
 	res: Response,
 	next: Function
 ) => {
-	console.error('Something went wrong', err);
+	if (err instanceof RequestValidationError) {
+		console.error('handling this error as a request validation error');
+		const formattedErrors = err.errors.map(error => {
+			return { message: error.msg, field: error.param };
+		});
+		return res.status(400).send({ errors: formattedErrors });
+	}
+
+	if (err instanceof DatabaseConnectionError) {
+		console.error('handling this error as a db validation error');
+		return res.status(500).send({ errors: [ { message: err.reason } ] });
+	}
+
 	res.status(400).send({
-		message: err.message
+		errors: [{ message: 'Something went wrong' }]
 	});
 };
