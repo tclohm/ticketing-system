9fb71f4fa7533b7db7895bec34941c0323d0efdb
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2022-02-22 11:09:50 -0800

    added typescript interface to describe what the User Document has

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -6,6 +6,12 @@ interface UserAttrs {
 	password: string;
 }
 
+// MARK: Interface describe the properties the User model has
+interface UserModel extends mongoose.Model<any> {
+	build(attrs: UserAttrs): any;
+}
+
+
 const userSchema = new mongoose.Schema({
 	email: {
 		type: String,
@@ -17,11 +23,22 @@ const userSchema = new mongoose.Schema({
 	}
 });
 
-const User = mongoose.model('User', userSchema);
-
-// MARK: -- getting typescript involved
-const buildUser = (attrs: UserAttrs) => {
+// MARK: -- getting typescript involved, custom function in mongoose
+userSchema.statics.build = (attrs: UserAttrs) => {
 	return new User(attrs)
 }
 
-export { User, buildUser };
+// MARK: -- describes properties that a User Document has
+interface UserDoc extends mongoose.Document {
+	email: string;
+	password: string;
+}
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+User.build({
+	email: "heello@goma.com",
+	password: "coolbeans"
+})
+
+export { User };
