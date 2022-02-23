ad33ffa20e7f628f2f6dfddc2532154b71e0ca25
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2022-02-23 13:48:42 -0800

    user.ts toJSON transforming to json removing _id and password

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -22,6 +22,15 @@ const userSchema = new mongoose.Schema({
 		type: String,
 		required: true
 	}
+}, {
+	toJSON: {
+		transform(doc, ret) {
+			ret.id = ret._id;
+			delete ret._id;
+			delete ret.password;
+			delete ret.__v;
+		}
+	}
 });
 
 // MARK: -- middleware before 'save' occurs
