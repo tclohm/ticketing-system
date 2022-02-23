015ea5e7f26cab3c3c05242600d36094b20303f4
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2022-02-23 14:21:33 -0800

    validate-request middleware

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,57 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { validateRequest } from '../middlewares/validate-request';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
+import Password from '../services/password';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-	res.send("hello from signin")
-})
+router.post('/api/users/signin',
+	[
+		body('email')
+			.isEmail()
+			.withMessage('Email must be valid'),
+		body('password')
+			.trim()
+			.notEmpty()
+			.withMessage('Please provide a password')
+
+	],
+	validateRequest,
+	async (req: Request, res: Response) => {
+		const { email, password } = req.body;
+
+		const existing = await User.findOne({ email });
+
+		if (!existing) {
+			throw new BadRequestError('Creds failed');
+		}
+
+		const matched = await Password.compare(existing.password, password);
+
+		if (!matched) {
+			throw new BadRequestError('Creds failed');
+		}
+
+		// MARK: -- generate jwt
+		const userJWT = jwt.sign(
+		{
+			id: existing.id,
+			email: existing.email
+		}, 
+		process.env.JWT_KEY!
+		);
+
+		// MARK: -- store on session object
+		req.session = {
+			jwt: userJWT
+		};
+
+		res.status(200).send(existing);
+	}
+);
 
 export{ router as signinRouter };
