9ac9acd69125e532d05856f0c957acc0e70b1f94
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2022-03-01 08:33:13 -0800

    getServerSideProps

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,30 @@
-const Root = ({ color }) => {
-	console.log(color)
+import axios from 'axios';
+
+const Root = ({ currentUser }) => {
+	console.log(currentUser)
 	return <h1>Root</h1>
 }
 
-Root.getInitialProps = async (ctx) => {
-	return {
-		color: 'orange'
+// MARK: -- Function
+export const getServerSideProps = async ({ req }) => {
+	if (typeof window === 'undefined') {
+		// MARK: -- Server
+		// http://servicename.namespace.svc.cluster.local
+		const { data } = axios.get(
+			'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', 
+			{
+				withCredentials: true,
+				headers: req.headers
+			}
+		).catch(err => {
+			console.log(err)
+			return { props: err }
+		});
+		return { props: data }
+	} else {
+		// MARK: -- Browser
+		const { data } = await axios.get('/api/users/currentuser');
+		return { props: data }
 	}
 }
 
