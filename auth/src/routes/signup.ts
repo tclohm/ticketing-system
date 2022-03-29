c2fbbfa66b23ee7dc550b11771c932aaeb1ae910
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2022-03-29 13:16:43 -0700

    using @eventspaceticketing/common

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,10 +1,9 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
+import { validateRequest, BadRequestError } from '@eventspaceticketing/common';
 
 import { User } from '../models/user';
-import { validateRequest } from '../middlewares/validate-request';
-import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
