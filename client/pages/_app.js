b9db110968bb36190f8cd1d7d8844864dda65c7b
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-04-02 16:19:36 -0700

    working on auth in next

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,30 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import buildClient from '../api/build-client';
 
-import Header from '../components/header';
-
-const AppComponent = ({ Component, pageProps, currentUser }) => {
+const AppComponent = ({ Component, pageProps }) => {
 	return (
-		<div>
-			<Header currentUser={currentUser} />
-			<Component {...pageProps} />
-		</div>
-	)
-		
+		<Component {...pageProps} />
+	)	
 	
-};
-
-AppComponent.getInitialProps = async (ctx) => {
-	const { data } = await buildClient(ctx.ctx).get('/api/users/currentuser');
-	let pageProps = {};
-	if (ctx.Component.getInitialProps) {
-		pageProps = await ctx.Component.getInitialProps(ctx.ctx)
-	}
-	
-	return {
-		pageProps,
-		...data
-	};
 };
 
 export default AppComponent;
