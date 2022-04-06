295c92002d32ce006705c63c6061765efbd22cae
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-04-06 12:54:04 -0700

    testing modified

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -3,7 +3,6 @@ import { app } from '../../app';
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
 	const response = await request(app).post('/api/tickets').send({});
-
 	expect(response.status).not.toEqual(404);
 });
 
