84c6e4005f6a124a5e07aeeb8cfa95a25a06c353
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-04-12 20:16:26 -0700

    listener is abstract and extends Event

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,14 +1,19 @@
 import { Listener } from './listener';
 import { Message } from 'node-nats-streaming';
+import { TicketCreatedEvent } from './ticket-created-event';
+import { Subjects } from './subjects';
 
 
-export class TicketCreatedListener extends Listener {
-	subject = 'ticket:created';
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+	readonly subject: Subjects.TicketCreated = Subjects.TicketCreated;
 	queueGroupName = 'payments-service';
 
 	// MARK: -- business logic
 	onMessage(data: any, msg: Message) {
 		console.log('Event data', data);
+
+		console.log(data.name);
+		console.log(data.cost);
 
 		msg.ack();
 	}
