48c6806f7e3a3adc41772de747c015294c8b50fc
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-04-15 12:39:37 -0700

    connectin nats to ticket

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@eventspaceticketing/common';
 
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+
 import { Ticket } from '../models/ticket';
 
 const router  = express.Router();
@@ -23,6 +25,13 @@ router.post(
 			userId: req.currentUser!.id
 		});
 		await ticket.save();
+
+		new TicketCreatedPublisher(client).publish({
+			id: ticket.id,
+			title: ticket.title,
+			price: ticket.price,
+			userId: ticket.userId
+		});
 
 		res.status(201).send(ticket);
 });
