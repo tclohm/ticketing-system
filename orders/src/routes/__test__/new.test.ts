9abfb42a9adfc01f5ab32788d1f2c862f4c992ea
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-04-22 08:56:43 -0700

    adding tests

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -1,79 +1,21 @@
+import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../../app';
-import { Ticket } from '../../models/ticket';
-import { natsWrapper } from '../../nats-wrapper';
 
-it('has a route handler listening to /api/tickets for post requests', async () => {
-	const response = await request(app).post('/api/tickets').send({});
-	expect(response.status).not.toEqual(404);
-});
-
-it('can only be accessed if user is signed in', async () => {
-	await request(app).post('/api/tickets').send({}).expect(401)
-});
-
-it('returns a status other than 401 if signed in', async () => {
-	const response = await request(app).post('/api/tickets').set('Cookie', global.signin()).send({});
-	expect(response.status).not.toEqual(401);
-})
-
-it('returns an error if an invalid title is provided', async () => {
-	await request(app)
-	.post('/api/tickets')
-	.set('Cookie', global.signin())
-	.send({
-		title: '',
-		price: 10
-	})
-	.expect(400)
-
-});
-
-it('returns an error if an invalid price is provided', async () => {
-	await request(app)
-	.post('/api/tickets')
-	.set('Cookie', global.signin())
-	.send({
-		title: 'valid title',
-		price: -20
-	})
-	.expect(400)
-});
-
-
-it('creates a ticket with valid inputs', async () => {
-	let tickets = await Ticket.find({});
-	expect(tickets.length).toEqual(0);
-
-	const title = 'Timeout! The 88 Tour!';
+it('returns an error if the ticket does not exist', async () => {
+	const ticketId = new mongoose.Types.ObjectId();
 
 	await request(app)
-		.post('/api/tickets')
+		.post('/api/orders')
 		.set('Cookie', global.signin())
-		.send({
-			title,
-			price: 20
-		})
-		.expect(201);
-
-	tickets = await Ticket.find({});
-	expect(tickets.length).toEqual(1);
-	expect(tickets[0].price).toEqual(20);
-	expect(tickets[0].title).toEqual(title);
+		.send({ ticketId })
+		.expect(404);
 });
 
-it('publishes an event', async () => {
+it('returns an error if the ticket already reserved', async () => {
+	
+});
 
-	const title = 'Timeout! The 88 Tour!';
+it('reserves a ticket', async () => {
 
-	await request(app)
-		.post('/api/tickets')
-		.set('Cookie', global.signin())
-		.send({
-			title,
-			price: 20
-		})
-		.expect(201);
-
-	expect(natsWrapper.client.publish).toHaveBeenCalled();
-})
+});
