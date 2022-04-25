28f829088cb9dd8816cb6e0be10ffda0110214ec
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-04-25 10:41:56 -0700

    orders/publisher edit and create test to see publisher is being called

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -6,6 +6,9 @@ import { body } from 'express-validator';
 import { Ticket } from '../models/ticket';
 import { Order } from '../models/order';
 
+import { OrderCreatedPublisher } from '../events/publishers/order-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
+
 const router = express.Router();
 
 // MARK:
@@ -50,6 +53,17 @@ async (req: Request, res: Response) => {
 	await order.save();
 
 	// Publish event -- order created
+	new OrderCreatedPublisher(natsWrapper.client).publish({
+		id: order.id,
+		status: order.status,
+		userId: order.userId,
+		expiresAt: order.expiresAt.toISOString(),
+		ticket: {
+			id: ticket.id,
+			price: ticket.price,
+		},
+	});
+
 
 	res.status(201).send(order);
 });
