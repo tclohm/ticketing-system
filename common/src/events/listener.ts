2b1789aba8b207df03e706a1ce02c3b802870b44
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-04-26 07:26:59 -0700

    added pipe to listener in common mod

--- common/src/events/listener.ts
+++ common/src/events/listener.ts
@@ -35,7 +35,7 @@ export abstract class Listener<T extends Event> {
 
 		subscription.on('message', (msg: Message) => {
 			console.log(
-				`Message received: ${this.subject} ${this.queueGroupName}`
+				`Message received: ${this.subject} | ${this.queueGroupName}`
 			);
 
 			const parsedData = this.parseMessage(msg)
