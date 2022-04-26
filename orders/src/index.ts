963521455f5f536b5fa70ee6843411e694e4cc77
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-04-25 21:04:02 -0700

    update-listener in orders and added listening to index

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -4,6 +4,9 @@ import { app } from './app';
 
 import { natsWrapper } from './nats-wrapper';
 
+import { TicketCreatedListener } from './events/listeners/ticket-created-listener';
+import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener'
+
 const start = async () => {
 
 
@@ -38,6 +41,9 @@ const start = async () => {
 
 		process.on('SIGINT', () => natsWrapper.client.close());
 		process.on('SIGTERM', () => natsWrapper.client.close());
+
+		new TicketCreatedListener(natsWrapper.client).listen();
+		new TicketUpdatedListener(natsWrapper.client).listen();
 
 		await mongoose.connect(process.env.MONGO_URI);
 	} catch (err) {
