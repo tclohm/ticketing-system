ec8c8f25f4977eaead46432cca14a5b4de491958
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-04-27 08:35:19 -0700

    adding tests for order-created-listener in tickets

---
+++ tickets/src/events/listeners/__test__/order-created-listener.test.ts
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+import { Message } from 'node-nats-streaming';
+import { OrderCreatedListener } from '../order-created-listener';
+import { natsWrapper } from '../../../nats-wrapper';
+import { Ticket } from '../../../models/ticket';
+import { OrderCreatedEvent. OrderStatus } from '@eventspaceticketing/common';
+
+const setup = async () => {
+	// create instance of listener
+	const listener = new OrderCreatedListener(natsWrapper.client);
+
+	// create and save ticket
+	const ticket = Ticket.build({
+		title: 'Marty and Doc: Flying through time',
+		price: 10000,
+		userId: 'mongoose#arbvalue'
+	});
+
+	await ticket.save();
+
+	// create fake data event
+	const data: OrderCreatedEvent['data'] = {
+		id: new mongoose.Types.ObjectId().toHexString(),
+		version: 0,
+		status: OrderStatus.Created,
+		userId: ticket.userId,
+		expiresAt: '40129312',
+		ticket: {
+			id: ticket.id,
+			price: ticket.price
+		}
+
+	};
+
+	// @ts-ignore
+	const msg: Message = {
+		ack: jest.fn()
+	};
+
+	return { listener, ticket, data, msg };
+};
