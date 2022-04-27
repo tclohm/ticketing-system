2c8a18451dbc43feeae6dff51ff93c5dc9756b0d
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-04-27 09:40:33 -0700

    listener publishing own event in tickets/order-created-listener

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -2,8 +2,7 @@ import { Message } from 'node-nats-streaming';
 import { Listener, OrderCreatedEvent, Subjects } from '@eventspaceticketing/common';
 import { queueGroupName } from './queue-group-name';
 import { Ticket } from '../../models/ticket';
-
-import { TicketCreatedPublisher } from '../publishers/ticket-created-publisher';
+import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publisher';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 	subject: Subjects.OrderCreated = Subjects.OrderCreated;
@@ -23,6 +22,16 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
 		// save ticket
 		await ticket.save();
+
+		// listener that publishs it own event
+		await new TicketUpdatedPublisher(this.client).publish({
+			id: ticket.id,
+			version: ticket.version,
+			title: ticket.title,
+			price: ticket.price,
+			userId: ticket.userId,
+			orderId: ticket.orderId
+		});
 
 		// ack the message
 		msg.ack();
