5f0299c65c8977e38f1c990998ff75d8ad449b9f
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-04-26 20:04:01 -0700

    fixing tests in orders

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -18,6 +18,7 @@ it('returns an error if the ticket does not exist', async () => {
 
 it('returns an error if the ticket already reserved', async () => {
 	const ticket = Ticket.build({
+		id: new mongoose.Types.ObjectId(),
 		title: 'Cool beans',
 		price: 40
 	});
@@ -41,6 +42,7 @@ it('returns an error if the ticket already reserved', async () => {
 
 it('reserves a ticket', async () => {
 	const ticket = Ticket.build({
+		id: new mongoose.Types.ObjectId().toHexString(),
 		title: 'Cool beans',
 		price: 40
 	});
@@ -56,6 +58,7 @@ it('reserves a ticket', async () => {
 
 it('emits an order created event', async () => {
 		const ticket = Ticket.build({
+		id: new mongoose.Types.ObjectId().toHexString(),
 		title: 'Cool beans',
 		price: 40
 	});
