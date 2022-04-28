68462898b9e689c109bc3fd280d4149d20ab2996
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-04-28 16:54:10 -0700

    dont allow update ticket when reserved and test

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -125,5 +125,25 @@ it('publishes an event', async () => {
 });
 
 it('rejects updates if the ticket is reserved', async () => {
+	const cookie = global.signin();
 
+	const response = await request(app)
+		.post('/api/tickets')
+		.set('Cookie', cookie)
+		.send({
+			title: 'cool beans',
+			price: 20
+		});
+
+	const ticket = await Ticket.findById(response.body.id);
+	ticket!.set({ orderId: new mongoose.Types.ObjectId().toHexString() })
+	await ticket!.save();
+
+	await request(app)
+		.put(`/api/tickets/${response.body.id}`)
+		.set('Cookie', cookie)
+		.send({
+			title: 'bloop',
+			price: 30
+	}).expect(200);
 });
