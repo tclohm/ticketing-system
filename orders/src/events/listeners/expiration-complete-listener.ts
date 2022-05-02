9d920393179e95f6e4aa5aae7d9f2809ca49df1d
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-05-02 14:04:26 -0700

    publisher in listener

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -2,13 +2,14 @@ import { Message } from 'node-nats-streaming';
 import { Subjects, Listener, ExpirationCompleteEvent, OrderStatus } from '@eventspaceticketing/common';
 import { Order } from '../../models/order';
 import { queueGroupName } from './queue-group-name';
+import { OrderCancelledPublisher } from '../publishers/order-cancelled-publisher';
 
 export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
 	subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete
 	queueGroupName = queueGroupName
 
 	async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
-		const order = await Order.findById(data.orderId);
+		const order = await Order.findById(data.orderId).populate('ticket');
 
 		if (!order) {
 			throw new Error('Order not found');
@@ -16,7 +17,17 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
 
 		order.set({
 			status: OrderStatus.Cancelled,
-			ticket: null,
 		});
+
+		await order.save();
+		await new OrderCancelledPublisher(this.client).publish({
+			id: order.id,
+			version: order.version,
+			ticket: {
+				id: order.ticket.id
+			}
+		});
+
+		msg.ack();
 	}
 }
