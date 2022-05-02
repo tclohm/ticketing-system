3cf33063f670ed3d37603c5aeebe1768400ee35f
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-05-02 13:48:18 -0700

    listener for order cancelled

---
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,22 @@
+import { Message } from 'node-nats-streaming';
+import { Subjects, Listener, ExpirationCompleteEvent, OrderStatus } from '@eventspaceticketing/common';
+import { Order } from '../../models/order';
+import { queueGroupName } from './queue-group-name';
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+	subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete
+	queueGroupName = queueGroupName
+
+	async onMessage(data: ExpirationCompleteEvent['data'], msg: Message) {
+		const order = await Order.findById(data.orderId);
+
+		if (!order) {
+			throw new Error('Order not found');
+		}
+
+		order.set({
+			status: OrderStatus.Cancelled,
+			ticket: null,
+		});
+	}
+}
