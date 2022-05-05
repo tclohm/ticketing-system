9ee502105103f00337f15b0ae22e3b3ea0771096
Taylor Lohman <taylor.lohman.tl@gmail.com>
2022-05-05 16:55:09 -0700

    payment route/new and test and setup for optional id in sigin

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,6 +1,13 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
-import { requireAuth, validateRequest, BadRequestError, NotFoundError } from '@eventspaceticketing/common';
+import { 
+	requireAuth, 
+	validateRequest, 
+	BadRequestError, 
+	NotFoundError, 
+	NotAuthorizedError, 
+	OrderStatus 
+} from '@eventspaceticketing/common';
 import { Order } from '../models/order';
 
 const router = express.Router();
@@ -16,6 +23,22 @@ router.post('/api/payments',
 			.isEmail()
 	], 
 async (req: Request, res: Response) => {
+	const { token, orderId } = req.body;
+
+	const order = await Order.findById(orderId);
+
+	if (!order) {
+		throw new NotFoundError();
+	}
+
+	if (order.userId !== req.currentUser!.id) {
+		throw new NotAuthorizedError();
+	}
+
+	if (order.status === OrderStatus.Cancelled) {
+		throw new BadRequestError('Cannot pay for cancelled order');
+	}
+
 	res.send({ success: true });
 });
 
